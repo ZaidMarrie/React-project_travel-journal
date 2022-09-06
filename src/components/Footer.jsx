0e7ac564e7aaae2ae3745acbb4af8e0327ee4427
Zaid Marrie <zaidmarrie27@gmail.com>
2022-09-06 10:14:51 +0200

    build travel journal
    Build the travel journal app using the latest version of react (18.2.0) and vite.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<footer className="footer flex align-items-center">
+			<a
+				href="https://github.com/ZaidMarrie/travel-journal-solo-project"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				Project source code
+			</a>
+		</footer>
+	);
+}
+
+export default Footer;
